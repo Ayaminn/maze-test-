45e1fe64c017c696c88577fb68221d6d73ec4a2c
Ayaminn <ayamin.imai@gmail.com>
2016-01-03 15:44:07 +0900

    First

---
+++ Assets/Scripts/switchScene.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class  switchScene : MonoBehaviour {
+	public GameObject unityChan;
+	public Material sun;
+	public Material night;
+	public bool nig = false;
+	public GameObject white;
+	public GameObject sunblack;
+	public GameObject nigblack;
+	public bool nigw = false;
+	public bool sunw = false;
+
+	public static  switchScene _swith;
+	void Awake ()
+	{
+
+		_swith = this;
+//		if (instance == null) {
+//			
+//			
+//			DontDestroyOnLoad (gameObject);
+//		}
+//		else {
+//			
+//			Destroy (gameObject);
+//		}
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 if(Input.GetKeyDown(KeyCode.A)){
+			    if(nig == false){
+				nigw = true;
+				RenderSettings.skybox = night;
+				nig = true;
+			}else{
+				nigw = true;
+				RenderSettings.skybox = sun;
+				nig = false;
+			}
+		}
+		if (nigw == true && white.transform.position.x > -3.5f){
+			white.transform.localPosition += new Vector3 (-0.05f, 0, 0);
+			nigblack.transform.localPosition += new Vector3 (0.05f, 0, 0);
+		} else if (sunw == true && white.transform.position.x > -3.5f){
+			white.transform.position += new Vector3 (-0.005f, 0, 0);
+			sunblack.transform.position += new Vector3 (0.005f, 0, 0);
+		}
+	}
+}
---
+++ Assets/Scripts/wall.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wall : MonoBehaviour {
+	Vector3 night;
+	Vector3 deffault;
+
+	bool upFlag = false;
+
+	// Use this for initialization
+	void Start () {
+
+		deffault = transform.position;
+		night = transform.position;
+
+	}
+
+	void OnCollisionEnter(Collision col){ 
+		if(switchScene._swith.nig ==  true){
+			night =new  Vector3 (deffault.x, deffault.y + 0.005f * 100, deffault.z);
+			StartCoroutine ("Speed", 0.005f);
+
+		}
+	}
+
+	IEnumerator Speed(float spd){
+
+		upFlag = true;
+
+
+		for (int i = 0; i < 100; i++){
+			transform.position +=  new Vector3(0, spd, 0);
+
+			yield return new WaitForSeconds (0.05f);
+		}
+
+
+		upFlag = false;
+		yield return  null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(switchScene._swith.nig ==  false){
+			gameObject.transform.position = new Vector3 (3.5f, 0.0f, -4.3f);
+		}else{
+			if(upFlag == false) {
+				gameObject.transform.position = night;
+			}
+		}
+	}
+}
